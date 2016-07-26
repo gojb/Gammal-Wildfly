64caca444efa625fca93346c03c77ddce20ed676
Jakob <jakob_b99@live.se>
2016-07-26 23:14:09 +0200

    Signed-off-by: Jakob <jakob_b99@live.se>

--- src/main/java/Skepp.java
+++ src/main/java/Skepp.java
@@ -67,8 +67,8 @@ public class Skepp {
 				String string2 = new String(skepp.session.toString().trim());
 				String string3 = new String(message.split(" ")[1].trim());
 				
-				skickaAlla(skepp.session.toString());
-				skickaAlla(message.split(" ")[1]);
+				skickaAlla(string);
+				skickaAlla(string2);
 				if (string2.equals(string3)) {
 					skickaAlla("hej");
 				}
